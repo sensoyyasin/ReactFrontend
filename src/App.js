380f3700baede46fedbfe0cf332a2455905c1c9a
Yasin Şensoy <73845925+sensoyyasin@users.noreply.github.com>
2021-06-06 00:51:46 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
+import {Container} from 'semantic-ui-react'
 import './App.css';
 import Dashboard from './layouts/Dashboard';
 import 'semantic-ui-css/semantic.min.css'
+import Navi from './layouts/Navi';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <Navi/>
+      <Container className = "main">
+        <Dashboard/>
+      </Container>
     </div>
   );
 }
--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Button, Container, Dropdown, Menu } from 'semantic-ui-react'
+import { Button, Container, Menu } from 'semantic-ui-react'
 import CartSummary from './CartSummary'
 
 export default function Navi() {
     return (
         <div>
-            <Menu inverted fixed>
+            <Menu inverted fixed = "top">
                 <Container>
                 <Menu.Item
                     name='home'
@@ -15,13 +15,7 @@ export default function Navi() {
                 />
 
                 <Menu.Menu position='right'>
-                    <Dropdown item text='Language'>
-                        <Dropdown.Menu>
-                            <Dropdown.Item>English</Dropdown.Item>
-                            <Dropdown.Item>Russian</Dropdown.Item>
-                            <Dropdown.Item>Spanish</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
+                    <CartSummary/>
 
                     <Menu.Item>
                         <Button primary>Sign Up</Button>
